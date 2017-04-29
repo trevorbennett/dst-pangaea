701377e9d92fe73ab487a5ad5634f97114c656b2
Trevor Bennett <garbage@trevorbennett.us>
2017-04-28 23:58:23 -0400

    partial connectivity in the network achieved

--- scripts/map/levels/pangaea.lua
+++ scripts/map/levels/pangaea.lua
@@ -1,13 +1,14 @@
-roomList = {"Marsh","Forest","BarePlain","Plain","Clearing"};
+GLOBAL.require("map/terrain")
+
+roomList = {"Marsh","Forest","BarePlain","Plain","Clearing","BGSavanna", "BeeClearing", "Pondopolis", "BGGrass", "BGRocky", "Graveyard", "DeepForest", "CritterDen", "Rocky", "SlightlyMermySwamp",
+							"BeeClearing", "FlowerPatch", "BeeQueenBee", "PigKingdom", "ForestMole", "MoonbaseOne", "WalrusHut_Grassy", "BGMarsh", "BGChessRocky", "BGNoise", "BGDeepForest", "BGCrappyForest",
+							"SpiderForest", "BGForest", "BGGrassBurnt"}
 FORCE_CONNECTED = "ForceConnected"
 
 for i,v in ipairs(roomList) do
   AddRoomPreInit(i, function(room)
-    addConnectedTag(room)
+  	table.insert(room.tags, FORCE_CONNECTED)
+		print("Trevor" + room.tags)
     end
   )
 end
-
-function addConnectedTag(room)
-    table.insert(room.tags, FORCE_CONNECTED)
-    end
